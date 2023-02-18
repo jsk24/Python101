7fb1fdbfaf6d773e0ae40b518caa463c52e33c35
Jay Es Kaay <122607224+jsk24@users.noreply.github.com>
2023-02-18 19:40:33 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,10 @@
+Python 3.11.1 (v3.11.1:a7a450f84a, Dec  6 2022, 15:24:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello Uncle')
+Hello Uncle
+>>> print('Robert')
+Robert
+>>> print('Hello Robert')
+Hello Robert
+>>> print ('Hello Somkit')
+Hello Somkit
